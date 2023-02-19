077ee2031c7be2a742a96ea6d92d8055cd60835e
dunsd <dthomasparker@gmail.com>
2023-02-19 23:04:09 +0000

    Add win condition and check if already marker

--- javascript.js
+++ javascript.js
@@ -4,9 +4,6 @@ const player = (() => {
   const player1Marker = 'X';
   const player2Marker = 'O';
   let currentMarker = 'X';
-  const addScore = (score) => {
-    console.log('add score');
-  };
 
   const changePlayer = () => {
     if (currentPlayer === 1) {
@@ -25,33 +22,75 @@ const player = (() => {
   };
 
   return {
-    addScore,
     getMarker,
   };
 })();
 
 const gameBoard = (() => {
-  const rows = 3;
-  const columns = 3;
-  const board = [['', '', ''], ['', '', ''], ['', '', '']];
+  const board = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];
 
   const printBoard = () => {
     console.log(board);
   };
 
+  const checkIfMarker = (x, y) => {
+    if (board[y][x] === 'X' || board[y][x] === 'Y') {
+      return true;
+    }
+    return false;
+  };
+
   const addMarker = (e) => {
     const x = e.target.getAttribute('data-x');
     const y = e.target.getAttribute('data-y');
-    board[y][x] = 99;
-    console.log(board);
-    e.target.textContent = 'X';
+    if (checkIfMarker(x, y)) { return; }
+    board[y][x] = player.getMarker();
+    e.target.textContent = board[y][x];
+    gameFlow.checkResult();
   };
+
+  const getBoardValue = (x, y) => board[y][x];
 
   return {
     printBoard,
     addMarker,
+    getBoardValue,
   };
 })();
+
+const gameFlow = (() => {
+  let turn = 0;
+
+  const checkTurn = () => {
+    if (turn === 9) {
+      console.log('Tie game');
+    }
+  };
+
+  const checkResult = () => { // checks all winning combinations
+    if ((gameBoard.getBoardValue(0, 0) === gameBoard.getBoardValue(1, 1)
+    && gameBoard.getBoardValue(1, 1) === gameBoard.getBoardValue(2, 2))
+    || (gameBoard.getBoardValue(0, 0) === gameBoard.getBoardValue(0, 1)
+    && gameBoard.getBoardValue(0, 1) === gameBoard.getBoardValue(0, 2))
+    || (gameBoard.getBoardValue(0, 0) === gameBoard.getBoardValue(1, 0)
+    && gameBoard.getBoardValue(1, 0) === gameBoard.getBoardValue(2, 0))
+    || (gameBoard.getBoardValue(0, 2) === gameBoard.getBoardValue(1, 2)
+    && gameBoard.getBoardValue(1, 2) === gameBoard.getBoardValue(2, 2))
+    || (gameBoard.getBoardValue(2, 0) === gameBoard.getBoardValue(2, 1)
+    && gameBoard.getBoardValue(2, 1) === gameBoard.getBoardValue(2, 2))
+    || (gameBoard.getBoardValue(2, 0) === gameBoard.getBoardValue(1, 1)
+    && gameBoard.getBoardValue(1, 1) === gameBoard.getBoardValue(0, 2))) {
+      console.log('winner');
+    }
+    turn += 1;
+    checkTurn();
+  };
+
+  return {
+    checkResult,
+  };
+})();
+
 const container = document.querySelector('.board');
 const squares = container.querySelectorAll('.square');
 squares.forEach((square) => square.addEventListener('click', (e) => gameBoard.addMarker(e)));
