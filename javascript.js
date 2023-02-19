9d7a7505d44b842436ed209a443068e7acf3f739
dunsd <dthomasparker@gmail.com>
2023-02-19 21:58:07 +0000

    Change grid to html defined and assign x,y

--- javascript.js
+++ javascript.js
@@ -1,11 +1,9 @@
 // create player module
 const player = (() => {
-//   const _player1Score = 0;
-//   const _player2Score = 0;
-  let currentPlayer = 1;
+  let currentPlayer = 0;
   const player1Marker = 'X';
   const player2Marker = 'O';
-  const currentMarker = 'X';
+  let currentMarker = 'X';
   const addScore = (score) => {
     console.log('add score');
   };
@@ -13,34 +11,53 @@ const player = (() => {
   const changePlayer = () => {
     if (currentPlayer === 1) {
       currentPlayer = 0;
+      currentMarker = player1Marker;
     } else {
       currentPlayer = 1;
+      currentMarker = player2Marker;
     }
   };
+
+  const getMarker = () => {
+    const marker = currentMarker;
+    changePlayer();
+    return marker;
+  };
+
   return {
     addScore,
-    player1Marker,
-    player2Marker,
+    getMarker,
   };
 })();
 const container = document.querySelector('.board');
 
 const gameBoard = (() => {
-  const makeGrid = (gridSize) => {
-    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-    for (let i = 0; i < gridSize * gridSize; i += 1) {
-      const square = document.createElement('div');
-      square.classList.add('square');
-      square.addEventListener('click', (e) => {
-        e.target.textContent = 'test';
-      });
-      container.appendChild(square);
+  const rows = 3;
+  const columns = 3;
+  const board = [['', '', ''], ['', '', ''], ['', '', '']];
+
+  for (let i = 0; i < rows; i += 1) {
+    board[i] = [];
+    for (let j = 0; j < columns; j += 1) {
+      board[j] = [];
     }
-  };
+  }
+
+  //   const makeGrid = (gridSize) => {
+  //     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  //     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  //     for (let i = 0; i < gridSize * gridSize; i += 1) {
+  //       const square = document.createElement('div');
+  //       square.classList.add('square');
+  //       square.addEventListener('click', (e) => {
+  //         e.target.textContent = player.getMarker();
+  //       });
+  //       container.appendChild(square);
+  //     }
+  //   };
 
   return {
-    makeGrid,
+    // makeGrid,
   };
 })();
 
